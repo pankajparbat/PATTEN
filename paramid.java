b5b8e9f73300bf4560c7ab6498ec36c74f7df764
Pankaj Dashrath Parbat <121680162+pankajparbat@users.noreply.github.com>
2023-09-08 21:53:50 +0530

    Program to print full pyramid using

---
+++ paramid.java
@@ -0,0 +1,25 @@
+package DSA.patten;
+
+public class paramid {
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++)
+        {
+            for (int j=5;j>=i;j--)
+            {
+                System.out.print("  ");
+            }
+            for (int k=1;k<=i;k++)
+            {
+                System.out.print("* ");
+            }
+            for (int k=1;k<i;k++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
