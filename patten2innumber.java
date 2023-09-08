5f1003627fc44040cf4f4ffb5588945ba277ddbe
Pankaj Dashrath Parbat <121680162+pankajparbat@users.noreply.github.com>
2023-09-08 21:52:01 +0530

    Inverted half pyramid using numbers

---
+++ patten2innumber.java
@@ -0,0 +1,16 @@
+package DSA.patten;
+
+public class patten2innumber {
+    public static void main(String[] args) {
+
+        for (int i=5;i>0;i--)
+        {int k=1;
+            for (int j=0;j<i;j++)
+            {
+                System.out.print(" "+k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
